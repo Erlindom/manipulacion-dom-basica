c84a3906a4d726b88fa035163b7192cdb7fdfec4
Erlindom <erlind299@gmail.com>
2022-08-30 15:05:45 -0500

    Agregando .gitignore y primeros archivos

---
+++ dom.js
@@ -0,0 +1,14 @@
+const input1 = document.querySelector("#numero1");
+const form = document.querySelector("#form");
+const input2 = document.querySelector("#numero2");
+const btn = document.querySelector("#btncalcular");
+const suma = document.querySelector("#sumaid");
+const text = document.querySelector("#text");
+
+//addEventListener
+
+text.addEventListener("copy", copiar);
+
+function copiar() {
+  alert("copiar");
+}
